f05825073bb7e35d1bcf36518be7eccf7e224e92
erik <erikpersson0884@gmail.com>
2023-07-05 17:18:36 +0200

    commit message

---
+++ updateScript.bat
@@ -0,0 +1,13 @@
+git status
+
+git add *
+git commit -m "commit message"
+git push
+ssh placer@192.168.1.200
+timeout /t 4 >nul
+honeycomb
+cd /var/shop-node-server
+git pull
+
+timeout /t 7 >nul
+echo Done waiting for four seconds.
