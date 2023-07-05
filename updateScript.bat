6f4f19d233657fcd373ece597020ab7c4f2c966c
erik <erikpersson0884@gmail.com>
2023-07-05 17:22:32 +0200

    commit message

--- updateScript.bat
+++ updateScript.bat
@@ -4,10 +4,8 @@ git add *
 git commit -m "commit message"
 git push
 ssh placer@192.168.1.200
-timeout /t 4 >nul
+@REM timeout /t 4 >nul
 honeycomb
 cd /var/shop-node-server
 git pull
 
-timeout /t 7 >nul
-echo Done waiting for four seconds.
